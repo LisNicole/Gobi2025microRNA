eaff4244b70647f5bed359c18fc5d84bbdc5a340
lisaboerner <ge59puj@tum.de>
2025-02-05 20:14:58 +0100

    updated SCRAP.sh file to quality control check the input files. Changes to the input requirements to only sample directory, adapter file, and pair-end sequencing

--- SCRAP.sh
+++ SCRAP.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-while getopts d:a:p:f:r:m:g: flag
+while getopts d:a:p: flag
 do
     case "${flag}" in
         d) directory=${OPTARG};;  # Sample directory
         a) adapter_file=${OPTARG};;  # Adapter file
         p) paired_end=${OPTARG};;  # Paired-end sequencing (yes/no)
-        f) pre_filtered=${OPTARG};;  # Filter pre-miRNAs (yes/no)
-        m) miRBase_species_abbreviation=${OPTARG};;  # miRBase species
-        g) genome_species_abbreviation=${OPTARG};;  # Genome species
     esac
 done
 
@@ -16,30 +13,25 @@ done
 if [ -z "$directory" ]; then echo "Error: Path to sample directories not provided [-d]"; exit 1; fi
 if [ -z "$adapter_file" ]; then echo "Error: Path to adapter file not provided [-a]"; exit 1; fi
 if [ -z "$paired_end" ]; then echo "Error: Are samples paired-end? [-p]"; exit 1; fi
-if [ -z "$pre_filtered" ]; then echo "Error: Filter out pre-miRNAs and tRNAs? [-f]"; exit 1; fi
-if [ -z "$miRBase_species_abbreviation" ]; then echo "Error: miRBase species abbreviation not provided [-m]"; exit 1; fi
-if [ -z "$genome_species_abbreviation" ]; then echo "Error: Species genome abbreviation not provided [-g]"; exit 1; fi
 
 # Ensure paths end with /
 directory=$(echo "${directory}" | sed 's![^/]$!&/!')
-reference_directory=$(echo "${reference_directory}" | sed 's![^/]$!&/!')
 
 # Activate the virtual environment
 source .venv/bin/activate
 
-# Get sample name from adapter file (assuming first column contains sample names)
-sample=$(awk '!/^#/' ${adapter_file} | awk '{print $1}' | head -n 1)
+# Get sample name from adapter file
+sample=$(awk '!/^#/ {print $1; exit}' ${adapter_file})
 
 echo "Running test on sample: ${sample}"
 
 ### Quality Control (FastQC) ###
 mkdir -p ${directory}FastQC_Reports
 
-for file in $(ls ${directory}${sample}.fastq | awk '/fastq/')
-
+for file in ${directory}${sample}*.fastq
 do
     echo "Running FastQC on ${file}"
-    fastqc -o ${directory}FastQC_Reports ${directory}${sample}/${file}
+    fastqc -o ${directory}FastQC_Reports "${file}"
 done
 
 ### Quality Control (MultiQC) ###
