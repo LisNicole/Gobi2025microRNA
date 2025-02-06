dea93b161e09e82eda1839f78f82a274b02239f2
Juli <julian.poschenrieder@web.de>
2025-02-06 10:33:40 +0100

    start of adapter read in

--- bin/SCRAP.sh
+++ bin/SCRAP.sh
@@ -39,3 +39,13 @@ wget https://gtrnadb.ucsc.edu/download/gtRNAdb.fasta -O ${directory}GtRNAdb.fast
 echo "GtRNAdb.fasta downloaded successfully!"
 
 echo "Test completed successfully!"
+
+
+for sample in $samples
+do
+
+  # Extract adapter and barcode sequences from adapter file
+  five_prime_adapter=$(awk -v var1=$sample '$1==var1' ${adapter_file} | awk -F '\t' '{print $2}')
+  three_prime_adapter=$(awk -v var1=$sample '$1==var1' ${adapter_file} | awk -F '\t' '{print $3}')
+  five_prime_barcode=$(awk -v var1=$sample '$1==var1' ${adapter_file} | awk -F '\t' '{print $4}')
+  three_prime_barcode=$(awk -v var1=$sample '$1==var1' ${adapter_file} | awk -F '\t' '{print $5}')
